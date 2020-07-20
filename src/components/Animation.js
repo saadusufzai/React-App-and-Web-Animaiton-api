749d99ffb8265a9ab74868d286000d2993690c15
Saad Aslam <saadusufzai@gmail.com>
2020-07-20 21:14:24 +0500

    Animation is working

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,48 +1,96 @@
 import React from 'react'
 import classes from './Animation.module.css'
 import fighter from '../images/3.png'
-import runner from '../images/run.png'
 import useWebApnimation from '@wellyshen/use-web-animations'
 
+import { keyHandler, KEYPRESS } from 'react-key-handler';
 const Animations = () => {
-    const {ref:figure, getAnimation:j1 } = useWebApnimation({
+
+    const {ref:figure, getAnimation } = useWebApnimation({
             keyframes:[
                 {transform:'translateX(0)'},
                 {transform:'translateX(-100%)'},
             ],
             timing:{
-                duration:1000,       
+                duration:800,       
                 easing:'steps(6,end)',
                 playbackRate: 1,
+                iterations:Infinity
+                
             }
     })
-    const {ref:run } = useWebApnimation({
-            keyframes:[
-                {transform:'translateX(0)'},
-                {transform:'translateX(-100%)'},
-            ],
-            timing:{
-                duration:5000,
-                iterations:Infinity,
-                easing:'steps(6,end)',
-                playbackRate: 1,
-            }
-    })
+   
+    const play = () => {
+        getAnimation().play();
+      };
+    
+      const pause = () => {
+        figure.current.style.bottom = '5em'
+        getAnimation().pause();
+      };
+    
+      const reverse = () => {
+        getAnimation().reverse();
+      };
+    
+      const cancel = () => {
+        getAnimation().cancel();
+      };
+    
+      const finish = () => {
+        getAnimation().finish();
+      };
+    
+    const stay = ()=>{
+
+        figure.current.style.bottom = '5em'
+      
+    }
+
+
 
     const jump = ()=>{
-        
 
+        figure.current.style.bottom = '25em'
+        const stay = ()=>{
+
+            figure.current.style.bottom = '5em'
+          
+        }
+        setTimeout(()=>{
+            stay()
+        },500)
     }
+    const run = ()=>{
+
+        figure.current.style.bottom = '45em'
+      
+    }
+    const punch = ()=>{
+
+        figure.current.style.bottom = '65em'
+        const stay = ()=>{
+
+            figure.current.style.bottom = '5em'
+          
+        }
+        setTimeout(()=>{
+            stay()
+        },500)
+    }
+
+   
 
     return (
         <div >
             <div className={classes.box}>
               <div><img ref={figure} className={classes.fig} src={fighter} alt={'figher'}/></div>  
             </div>     
-            <div>
+            <div className={classes.buttons}>
+                <button  onClick={()=>stay()} >STAY</button>
                 <button onClick={()=>jump()}>JUMP</button>
-                <button>RUN</button>
-                <button>PUNCH`</button>
+                <button onClick={()=>run()}>RUN</button>
+                <button onClick={()=>punch()}>PUNCH`</button>
            </div>
         </div>
     )
